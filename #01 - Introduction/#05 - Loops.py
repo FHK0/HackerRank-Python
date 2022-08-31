541f7b8b68707084c68e5fee9e83cc1a39d761a2
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-08-31 17:21:54 +0300

    Create #05 - Loops.py

---
+++ #01 - Introduction/#05 - Loops.py
@@ -0,0 +1,10 @@
+# #################################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0                                 #
+#  Link: https://www.hackerrank.com/challenges/python-loops/problem               #
+# #################################################################################
+
+#  Solution:
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i ** 2)
