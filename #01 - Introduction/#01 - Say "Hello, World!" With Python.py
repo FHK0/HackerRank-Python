170182c2fbdd80eb963e38914cd1722e854388c0
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-08-31 15:52:58 +0300

    Create #01 - Say "Hello, World!" With Python.py

---
+++ #01 - Introduction/#01 - Say "Hello, World!" With Python.py
@@ -0,0 +1,8 @@
+# #############################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0                             #
+#  Link: https://www.hackerrank.com/challenges/py-hello-world/problem         #
+# #############################################################################
+
+#  Solution:
+if __name__ == '__main__':
+    print("Hello, World!")
