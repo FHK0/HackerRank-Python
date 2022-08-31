dff3c0d12c69020b6f3f0d5dd3ee6b0c366db103
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-08-31 17:00:46 +0300

    Create #04 - Division.py

---
+++ #01 - Introduction/#04 - Division.py
@@ -0,0 +1,10 @@
+# #################################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0                                 #
+#  Link: https://www.hackerrank.com/challenges/python-division/problem            #
+# #################################################################################
+
+#  Solution:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a // b}\n{a / b}")
