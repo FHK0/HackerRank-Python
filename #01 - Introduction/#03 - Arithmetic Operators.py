e7f68d6dfdf76e68491436b363b94fedeabca773
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-08-31 16:49:14 +0300

    Create #03 - Arithmetic Operators.py

---
+++ #01 - Introduction/#03 - Arithmetic Operators.py
@@ -0,0 +1,19 @@
+# #################################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0                                 #
+#  Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/forum  #
+# #################################################################################
+
+#  Solution:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a + b}\n{a - b}\n{a * b}")
+    
+    """
+    Alternative Solution:
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+   
+    """
