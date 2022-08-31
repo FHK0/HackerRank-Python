92ffd66f882db465518f0aafc6e7030728e41378
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-08-31 16:29:15 +0300

    Create #02 - Python If-Else.py

---
+++ #01 - Introduction/#02 - Python If-Else.py
@@ -0,0 +1,22 @@
+# #############################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0                             #
+#  Link: https://www.hackerrank.com/challenges/py-if-else/problem             #
+# #############################################################################
+
+#  Solution:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 or (n in range(6, 21)):
+        print("Weird")
+    else:
+        print("Not Weird")
