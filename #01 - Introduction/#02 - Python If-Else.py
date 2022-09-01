0c9895281d52ebf0ccb78c158599d0f14f14d5d3
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-09-01 11:24:53 +0300

    Update #02 - Python If-Else.py

--- #01 - Introduction/#02 - Python If-Else.py
+++ #01 - Introduction/#02 - Python If-Else.py
@@ -4,16 +4,6 @@
 # #############################################################################
 
 #  Solution:
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
 if __name__ == '__main__':
     n = int(input().strip())
     if n % 2 or (n in range(6, 21)):
