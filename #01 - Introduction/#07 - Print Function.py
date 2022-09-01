51113ee37f9aaca205dccf149f6f6f567891fcf0
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-09-01 21:04:15 +0300

    Create #07 - Print Function.py

---
+++ #01 - Introduction/#07 - Print Function.py
@@ -0,0 +1,10 @@
+# #################################################################################
+#  Author: fhk0 | GitHub: https://github.com/FHK0 | HackerRank: @fhk0x            #
+#  Link: https://www.hackerrank.com/challenges/python-print/problem               #
+# #################################################################################
+
+#  Solution:
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (1, n+1):
+        print(i, end="")
