34a21b0cdbdfc8ccc4b17efdd7ef1efe225ff07f
FHK0 <46722046+FHK0@users.noreply.github.com>
2022-09-05 20:40:39 +0300

    Update sWAP cASE.py

--- #03 - Strings/sWAP cASE.py
+++ #03 - Strings/sWAP cASE.py
@@ -3,7 +3,7 @@
 #  Link: https://www.hackerrank.com/challenges/swap-case/problem                  #
 # #################################################################################
 
-#  Solution: [Opinion: This solution is so lazy. There is no need to define another function called swap_case() if you don't let me to create my own method.]
+#  Solution: [Opinion: This solution is so lazy. There is no need to define another function called swap_case() if you want me to use swapcase() method.]
 def swap_case(s):
     return s.swapcase()
 
